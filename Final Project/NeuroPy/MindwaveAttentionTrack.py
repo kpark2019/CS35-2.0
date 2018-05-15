767a017c98a14cda0c9648acd95599e407718abd
jjkatara1 <33379798+jjkatara1@users.noreply.github.com>
2018-05-14 18:02:30 -0700

    Add files via upload

---
+++ Final Project/NeuroPy/MindwaveAttentionTrack.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mindwave, time
+
+
+then = time.time()
+now = time.time()
+
+
+plt.axis([-2000, 2000, 0, 50])
+plt.ion()
+
+#connects the path(usb) and the specific serial number of your mindwave
+headset = mindwave.Headset('COM5', 'CC0E')
+time.sleep(2)
+
+#connecting the mindwave
+headset.connect()
+print ("Pairing...")
+
+while mindwave.status != "Paired!":
+    time.sleep(2)
+    if mindwave.status == "Standing by...":
+        mindwave.connect()
+        print ("Retrying...")
+print ("Paired!")
+
+#the graph of mindwave
+while True:
+    #time.sleep(.5)
+    print "Attention: %s, Meditation: %s, Raw: %s" % (headset.attention, headset.meditation, headset.raw_value)
+    now = time.time()
+    duration = now - then
+    duration_sec = duration % 60
+    x = str(headset.raw_value)
+    y = str(duration_sec)
+    plt.scatter(x, y)
+    plt.pause(1)
+
+
+
+while True:
+    plt.pause(1)
